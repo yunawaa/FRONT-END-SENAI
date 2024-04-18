53ee183f9bcee1deab0eeb6b93c20e4c9335507b
yunawaa <lxrdy000@gmail.com>
2024-04-18 09:48:13 -0300

    javascript
    aula1

---
+++ JavaScript/materialDeAula/js/script.js
@@ -0,0 +1,15 @@
+console.log('Olá mundo');
+
+//caixas de texto (input)
+//prompt = recebe um texto
+//window.prompt("Qual é o seu nome?");
+
+//alert = manda uma mensagem
+//window.alert("FRONT É TOP");
+
+// confirm = pergunta confirmar
+//window.confirm("Deseja baixar esse aquivo novamente?")
+
+var melancia = "Uma variável de teste";
+var ligado = true;
+var numero = 91;
