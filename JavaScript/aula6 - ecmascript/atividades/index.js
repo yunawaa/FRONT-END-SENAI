dd1efd6e60944553b8b8f8ed62e4cf4904cb5294
yunawaa <lxrdy000@gmail.com>
2024-05-06 11:50:19 -0300

    atividade de ecma
    aula6

---
+++ JavaScript/aula6 - ecmascript/atividades/index.js
@@ -0,0 +1,7 @@
+// 1) Fazer um input de pesquisa onde irá pesquisar se o nome de um jogo (em um array de pelomenos 3 elementos) existe, se existir, mande uma mensagem que ele foi encontrado, caso não, mande uma mensagem que não foi encontrado!
+// 2) Fazer um algoritmo com inputs onde possa implementar as funções de replace e replace all
+
+
+function pesquisar(){
+var games = ["Minecraft", "Valorant", "Wild Rift"];
+}
