40306474b2e50286d88a25decad69f2e30760a2b
yunawaa <lxrdy000@gmail.com>
2024-05-07 10:53:44 -0300

    atividade aula 05 ecma script
    correção

--- JavaScript/aula6 - ecmascript/atividades/index.js
+++ JavaScript/aula6 - ecmascript/atividades/index.js
@@ -2,6 +2,44 @@
 // 2) Fazer um algoritmo com inputs onde possa implementar as funções de replace e replace all
 
 
+const btnPesquisar = document.querySelector("#btnresposta");
+const jogos = ["Resident Evil","Dota 2", "Free Fire", "League of Legends","Castlevania"]
+btnPesquisar.addEventListener('click',pesquisar);
+
 function pesquisar(){
-var games = ["Minecraft", "Valorant", "Wild Rift"];
+    let jogopesquisado = document.querySelector("#inputresposta").value;
+    let respostapesquisa = jogos.includes(jogopesquisado);
+    let resposta = document.querySelector("#pesquisaresposta")
+
+    if(respostapesquisa == true){
+        resposta.innerHTML = `Jogo encontrado! Item da pesquisa: ${jogopesquisado}`;
+    }else{
+        resposta.innerHTML = `Jogo ${jogopesquisado} não foi encontrado!`
+    }
 }
+
+const btnreplace = document.querySelector("#btnreplace");
+const btnreplaceall = document.querySelector("#btnreplaceall");
+
+btnreplace.addEventListener("click",substituirum);
+btnreplaceall.addEventListener("click", substituirtodos);
+
+function substituirum(){
+    let texto = document.querySelector("#texto").value;
+    let achartermo = document.querySelector("#achartermo").value;
+    let trocartermo = document.querySelector("#trocartermo").value;
+
+    let textoatualizado = texto.replace(achartermo,trocartermo);
+    document.querySelector("#texto").value=textoatualizado;
+}
+
+function substituirtodos(){
+    let texto = document.querySelector("#texto").value;
+    let achartermo = document.querySelector("#achartermo").value;
+    let trocartermo = document.querySelector("#trocartermo").value;
+
+    let textoatualizado = texto.replaceAll(achartermo,trocartermo);
+    document.querySelector("#texto").value = textoatualizado;
+
+}
+
