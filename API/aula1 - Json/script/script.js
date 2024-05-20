1042f5f4e98b3618a00c7b5d750cf5f4e7a714e5
yunawaa <lxrdy000@gmail.com>
2024-05-20 11:50:48 -0300

    json aula 1
    introdução

---
+++ API/aula1 - Json/script/script.js
@@ -0,0 +1,25 @@
+const resposta = document.querySelector("#resposta")
+
+// fetch('dados.json').then((response) =>{
+//      response.json().then((carros) => {
+//         return carros.json()
+//      })
+// })
+
+var cep = document.querySelector("#cep")
+
+cep.addEventListener("focusout",pesquisar)
+
+function pesquisar(){
+
+fetch(`https://viacep.com.br/ws/${cep.value}/json/`).then((response) => {
+    return response.json()
+
+}).then(dados => {
+    resposta.innerHTML = `
+    <input id="localidade" value="${dados.localidade}">
+    <input id="logradouro" value="${dados.logradouro}">
+    <input id="bairro" value="${dados.bairro}">
+    `
+})
+}
